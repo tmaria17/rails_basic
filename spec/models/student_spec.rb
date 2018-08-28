efaeb8276450587c5760ab6d9d27d3bb9a13a32d
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-08-28 13:40:27 -0600

    Database set up for student and feature spec set up.

---
+++ spec/models/student_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe 'student show page' do
+  describe 'as a visitor' do
+    it "displays information for one student" do
+      student_1 = 
+      student_2 =
+      visit student_path(student)
+
+
+    end
+  end
+end
